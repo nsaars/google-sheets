1bd25b623caee6fb17983aad175f1f22fbcb1065
nsaars <mimalliktv04@gmail.com>
2022-08-01 16:22:41 +0500

    create sheets package

---
+++ data/config.py
@@ -0,0 +1,3 @@
+import pathlib
+
+auth_creds_file = str(pathlib.Path(__file__).parent.resolve()) + r'/creds.json'
---
+++ sheets/sheets.py
@@ -0,0 +1,62 @@
+import httplib2
+import apiclient
+from googleapiclient.discovery import Resource
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+class SheetsAuthorizer:
+    def __init__(self, credentials_file):
+        self.credentials = ServiceAccountCredentials.from_json_keyfile_name(
+            credentials_file,
+            ['https://www.googleapis.com/auth/spreadsheets',
+             'https://www.googleapis.com/auth/drive'])
+
+        http_auth = self.credentials.authorize(httplib2.Http())
+        self.service = apiclient.discovery.build('sheets', 'v4', http=http_auth)
+
+    async def get_service(self):
+        return self.service
+
+    async def get_credentials(self):
+        return self.credentials.to_json()
+
+
+class Table:
+    def __init__(self, service: Resource, spreadsheet_id):
+        self.service = service
+        self.spreadsheet_id = spreadsheet_id
+
+    async def read(self, sheet_name=None, start_ceil=None, end_ceil=None, major_dimension=None) -> dict:
+        if sheet_name is None:
+            if start_ceil is None or end_ceil is None:
+                raise TypeError("You have to define at least 'sheet_name' or 'start_ceil' and 'end_ceil'")
+
+        if sheet_name is None:
+            values_range = f'{start_ceil}:{end_ceil}'
+        elif start_ceil is None:
+            values_range = sheet_name
+        else:
+            values_range = f'{sheet_name}!{start_ceil}:{end_ceil}'
+
+        if major_dimension is None:
+            major_dimension = 'ROWS'
+
+        response: dict = self.service.spreadsheets().values().get(
+            spreadsheetId=self.spreadsheet_id,
+            range=values_range,
+            majorDimension=major_dimension
+        ).execute()
+
+        return response
+
+    async def write(self, data, start_ceil=None, end_ceil=None):
+        if start_ceil is None or end_ceil is None:
+            raise TypeError("You have to define both 'start_ceil' and 'end_ceil'")
+        response: dict = self.service.spreadsheets().values().update(
+            spreadsheetId=self.spreadsheet_id,
+            range=f"{start_ceil}:{end_ceil}",
+            valueInputOption="USER_ENTERED",
+            body={'values': data}
+        ).execute()
+        return response
+
